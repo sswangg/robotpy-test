768e9e4c53eefefeb2c87c487b2ab15d5cad5e5b
1076-3 <>
2022-01-29 17:48:59 -0500

    finish encoder_test.py

--- encoder_test.py
+++ encoder_test.py
@@ -1,21 +1,23 @@
 import wpilib
 from wpilib.drive import DifferentialDrive
-from rev import
+from rev import CANSparkMax, CANSparkMaxLowLevel
+from wpilib import SmartDashboard
+from networktables import NetworkTables
+
 
 class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
-        self.m_motor = wpilib.Spark(2)
+        self.m_motor = CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless)
+        self.m_motor.restoreFactoryDefaults()
         self.m_stick = wpilib.XboxController(0)
         self.m_encoder = self.m_motor.getEncoder()
-
-
-    def teleopInit(self):
-        """Executed at the start of teleop mode"""
-        self.myRobot.setSafetyEnabled(True)
+        self.sd = NetworkTables.getTable("LiveWindow")
+        NetworkTables.initialize(server="127.0.0.1")
 
     def teleopPeriodic(self):
-        """Runs the motors with tank steering"""
-        self.myRobot.tankDrive(self.leftStick.getY() * -1, self.rightStick.getY() * -1)
+        self.m_motor.set(self.m_stick.getRawAxis(2))
+        self.sd.putNumber("Encoder Position", self.m_encoder.getPosition())
+        self.sd.putNumber("Encoder Velocity", self.m_encoder.getVelocity())
 
 
 if __name__ == "__main__":
